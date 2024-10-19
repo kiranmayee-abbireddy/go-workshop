63091587fe139083d54dfeaea5246575bfad59c7
kiranmayee-abbireddy <kiranmayee118@gmail.com>
2024-10-19 11:49:34 +0530

    added new package , defined module and imported the sustom package in main function

---
+++ basics/basics.go
@@ -0,0 +1,91 @@
+package basics
+
+import "fmt"
+
+func RunBasics() {
+	fmt.Println("Hello, World!")
+	var age int = 21
+	var name string = "Kiranmayee"
+	var height float32 = 5.03
+	fmt.Printf("My name is %s and my age is %d\n", name, age)
+	fmt.Printf("My height is %.2f\n", height)
+
+	place := "Hyderabad"
+	runs := 100
+	fmt.Println("I am from", place)
+
+	if runs > 100 {
+		fmt.Println("You have scored a century")
+	} else {
+		fmt.Println("Yoy have scored less than a century")
+	}
+	//i is only available in for loop
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	i := 0
+	for i < 15 {
+		fmt.Println(i)
+		i++
+	}
+
+	j := 20
+	for {
+		if j > 25 {
+			break
+		}
+		fmt.Println("Infinite loop")
+		j++
+	}
+
+	nums := [5]int{100, 200, 300, 400, 500}
+	for index, value := range nums {
+		fmt.Println(index, value)
+	}
+
+	var x int64 = 10
+	var y int64 = 20
+	fmt.Println("Sum of the numbers is", add(x, y))
+	fmt.Println("The value of x is", x)
+
+	//Pointers
+	var xp *int64
+	xp = &x
+	fmt.Println("The value of xp is", xp)
+	xp = &y
+	fmt.Println("The value of xp is", xp)
+
+	//pass by reference
+	fmt.Println("The current value of x is", x)
+	fmt.Println("The value of x is", addByReference(&x, y))
+	fmt.Println("The value of x after add by reference is", x)
+
+	type Person struct {
+		name   string
+		age    int
+		height float32
+		place  string
+	}
+
+	p := Person{name: "Kiranmyee", age: 21, height: 5.03, place: "Hyderabad"}
+	fmt.Println(p.name, p.age, p.height, p.place)
+
+	q := new(Person)
+	q.name = "Mayee"
+	q.age = 21
+	q.height = 5.05
+	q.place = "Coimbatore"
+	fmt.Println(q)
+}
+
+func add(x int64, y int64) int64 {
+	x = x + 10
+	return x + y
+}
+
+func addByReference(x *int64, y int64) int64 {
+	*x = *x + 10
+	return *x + y
+}
